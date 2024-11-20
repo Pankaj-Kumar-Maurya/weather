99034c4dd18f5fd5a1ef7bd86ecf29a3ea257aa8
Pankaj Kumar Maurya <pankajmaurya872691@gmail.com>
2024-11-20 23:14:03 +0530

    Angular-19 Update and Material added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,10 @@ import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet,RouterLink,NavBarComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [RouterOutlet, RouterLink, NavBarComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Pankaj';
--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -2,11 +2,10 @@ import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-nav-bar',
-  standalone: true,
-  imports: [NgClass],
-  templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.css'
+    selector: 'app-nav-bar',
+    imports: [NgClass],
+    templateUrl: './nav-bar.component.html',
+    styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
   isMenuActive = false; // Menu visibility state
