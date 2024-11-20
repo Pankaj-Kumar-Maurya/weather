2348289b3646f1368fff5423b3991bb695a06fa6
Pankaj Kumar Maurya <pankajmaurya872691@gmail.com>
2024-11-20 23:54:04 +0530

    navbar-and Dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { RouterModule, Routes } from '@angular/router';
+import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 
 
 
 export const routes: Routes = [
-   
+   {
+    path:'Admin-Dashboard',
+    component:AdminDashboardComponent
+   }
     
   
 ];
--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,9 +1,10 @@
 import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
     selector: 'app-nav-bar',
-    imports: [NgClass],
+    imports: [NgClass,RouterLink,RouterLinkActive],
     templateUrl: './nav-bar.component.html',
     styleUrl: './nav-bar.component.css'
 })
